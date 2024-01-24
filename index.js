19dfa06894759b396735dde2ea93fd55cc3992bc
Eltrac <hi@guhub.cn>
2024-01-24 10:30:16 +0800

    feat(fetch): fetch markdown files in a dir

--- index.js
+++ index.js
@@ -6,8 +6,53 @@
  * an easy-to-use JSON API.
  */
 
-class Markson {
+// file system related
+import fs from 'fs'
+import path from 'path'
 
+// markdown parsing
+import { micromark } from 'micromark';
+import { gfm, gfmHtml } from 'micromark-extension-gfm';
+
+export default class Markson {
+    constructor(options = {
+        gfm: true
+    }) {
+
+        /**
+         * Dealing with options
+         */
+
+        // micromark options
+        // if gfm enabled, configure it
+        const micromarkOptions = options.gfm ? {
+            extensions: [gfm()],
+            htmlExtensions: [gfmHtml()]
+        } : null
+
+        /**
+         * Read a dir containing markdown files
+         * and generate JSON
+         * 
+         * @param {string} dir 
+         */
+
+        this.fetch = (dir) => {
+            const filenames = fs.readdirSync(dir);
+            
+            let array = [];
+            filenames.forEach((filename) => {
+                const content = fs.readFileSync(path.join(dir, filename), { encoding: 'utf-8'});
+                const html = micromark(content, micromarkOptions)
+
+                array.push({
+                    filename: filename,
+                    markdown: content,
+                    html: html
+                })
+            })
+
+            return array
+        }
+    }
 }
-
-module.exports = Markson;
